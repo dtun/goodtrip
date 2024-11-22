dbd5ca18169c497fad77e36d731fba428b76a77a
Danny Tunney <dtun@me.com>
2024-11-22 06:43:37 -0700

    Fix header visibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ import EnvCard from "./cards/envcard";
 
 export async function Header() {
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-white ">
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-white">
       <EnvCard />
       <Link href="/" rel="nofollow" className="mr-2 font-bold">
         GoodTrip
@@ -20,7 +20,7 @@ export async function Header() {
           "mr-auto font-normal"
         )}
       >
-        <span className="hidden md:flex">About</span>
+        <span className="md:flex">About</span>
       </Link>
     </header>
   );
