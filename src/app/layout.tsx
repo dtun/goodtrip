d75f962fd96e1016c99561479f1492b9252ecf30
Danny Tunney <dtun@me.com>
2024-11-22 06:58:51 -0700

    Update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { Header } from "@/components/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next.js AI Lite App",
-  description: "AI with Next and AI SDK",
+  title: "GoodTrip",
+  description: "Your Family Adventure, AI Perfected",
 };
 
 export default function RootLayout({
